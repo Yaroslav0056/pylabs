ee3731fd553e5b6da978423cf66f928f1c2000b4
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2024-11-28 23:16:03 +0200

    fixed lab4

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -9,28 +9,28 @@ class Chainsaw:
         self.weight = 0
         self.color = "Orange"
 
-    def getHours(self):
+    def get_hours(self):
         return self.hours
 
-    def setHours(self, hours):
+    def set_hours(self, hours):
         self.hours += hours
 
-    def getName(self):
+    def get_name(self):
         return self.__name
 
-    def setName(self, name):
+    def set_name(self, name):
         self.__name = name
 
-    def getPower(self):
+    def get_power(self):
         return self.__power
 
-    def setPower(self, power):
+    def set_power(self, power):
         self.__power = power
 
-    def getChain_speed(self):
+    def get_chain_speed(self):
         return self.__chain_speed
 
-    def setChain_speed(self, chain_speed):
+    def set_chain_speed(self, chain_speed):
         self.__chain_speed = chain_speed
 
     def __str__(self):
@@ -55,16 +55,16 @@ def main():
     chainsaw_1 = Chainsaw.create("Stihl MS 180", 1500, 9000, 7, "red", 10)
     chainsaw_2 = Chainsaw.create("Husqvarna 120", 1600, 8500, 6, "orange", 12)
     chainsaw_3 = Chainsaw.create("Makita UC4051A", 1800, 9200, 5, "blue", 13)
-    chainsaw_1.setHours(255)
-    chainsaw_2.setHours(22)
-    chainsaw_3.setHours(3)
+    chainsaw_1.set_hours(255)
+    chainsaw_2.set_hours(22)
+    chainsaw_3.set_hours(3)
 
     lst = [chainsaw_1, chainsaw_2, chainsaw_3]
     mx = lst[0]
-    for lst in lst:
-        if chainsaw_1.getHours() > mx.getHours():
+    for chainsaw in lst:
+        if chainsaw.get_hours() > mx.get_hours():
             mx = chainsaw_1
-    # print('Бензопила в якої найбільший час роботи :', mx)
+    print('Бензопила в якої найбільший час роботи :', mx)
     print(chainsaw_1, chainsaw_2, chainsaw_3, sep='\n')
 
 
